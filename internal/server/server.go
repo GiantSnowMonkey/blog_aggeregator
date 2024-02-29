e85b1340e3ad3a1a53e4bd1eae561a25015ccf8c
Sepehr Heidari <S.heydari0@gmail.com>
2024-02-29 16:31:52 -0500

    Added server boilerplate

---
+++ internal/server/server.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type Server struct {
+	port int
+}
+
+func NewServer() (*http.Server, error) {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		return nil, err
+	}
+	newServer := &Server{
+		port: port,
+	}
+
+	server := &http.Server{
+		Addr:        fmt.Sprint(":&v", newServer.port),
+		IdleTimeout: time.Minute,
+		Handler:     http.NotFoundHandler(),
+	}
+	return server, err
+}
