3a41e6f4c969be507b2ae16cddf32c37b756be6d
Sepehr Heidari <S.heydari0@gmail.com>
2024-03-03 13:10:39 -0500

    Initialized DB

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,11 +3,20 @@ package server
 import (
 	"net/http"
 
+	databse "github.com/GiantSnowMonkey/blog_aggeregator/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type apiConfig struct {
+	DB *databse.Queries
+}
+
 func (s Server) RegisterRoutes() http.Handler {
+	dbQueries := databse.LoadDB()
+	apiCfg := &apiConfig{
+		DB: dbQueries,
+	}
 	router := chi.NewRouter()
 	router.Use(customCors())
 	router.Use(middleware.Logger)
