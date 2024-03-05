713ac82acce2e1378a273b897b333fdaaad4d885
Sepehr Heidari <S.heydari0@gmail.com>
2024-03-05 10:53:44 -0500

    Updated handlerUsersCreate

--- internal/server/handler_users_create.go
+++ internal/server/handler_users_create.go
@@ -17,6 +17,7 @@ type RespondUser struct {
 	CreatedAt pgtype.Timestamp `json:"created_at"`
 	UpdatedAt pgtype.Timestamp `json:"update_at"`
 	Name      string           `json:"name"`
+	ApiKey    string           `json:"api_key"`
 }
 
 func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
@@ -28,23 +29,26 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 	err := decoder.Decode(&params)
 	if err != nil {
 		log.Println("Couldn't parse the params:", err)
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
 	}
 	timeNowTimeStamp := pgtype.Timestamp{Time: time.Now(), Valid: true}
-	userID := uuid.New()
-	println("UUID:", userID.String())
 	user, err := cfg.DB.CreateUser(context.Background(), databse.CreateUserParams{
-		ID:        pgtype.UUID{Bytes: [16]byte(userID), Valid: true},
+		ID:        pgtype.UUID{Bytes: [16]byte(uuid.New()), Valid: true},
 		CreatedAt: timeNowTimeStamp,
 		UpdatedAt: timeNowTimeStamp,
 		Name:      params.Name,
 	})
 	if err != nil {
-		log.Println(err)
+		log.Println("Couldn't create user:", err)
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
 	}
 	respondWithJSON(w, http.StatusOK, RespondUser{
 		ID:        user.ID,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
 		Name:      user.Name,
+		ApiKey:    user.ApiKey.String,
 	})
 }
