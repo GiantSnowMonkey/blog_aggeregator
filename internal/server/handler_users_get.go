a6d51300f5b3b0e209993b77facca41a13ce7b7b
Sepehr Heidari <S.heydari0@gmail.com>
2024-03-05 14:07:00 -0500

    Added handlerUsersGet

---
+++ internal/server/handler_users_get.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+	"net/http"
+
+	database "github.com/GiantSnowMonkey/blog_aggeregator/internal/database"
+)
+
+func (cfg *apiConfig) handlerUsersGet(w http.ResponseWriter, r *http.Request, user database.User) {
+	respondWithJSON(w, http.StatusOK, RespondUser{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Name:      user.Name,
+		ApiKey:    user.ApiKey.String,
+	})
+}
